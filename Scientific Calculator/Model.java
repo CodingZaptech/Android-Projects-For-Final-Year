8d0cc2f02d31782a147300f689746be8d664febb
CodingZap <help@codingzap.com>
2025-10-01 13:54:47 +0530

    Create Model.java

---
+++ Scientific Calculator/Model.java
@@ -0,0 +1,9 @@
+package com.example.calculator.model;
+
+/**
+ * Model class for basic and scientific calculations.
+ */
+public class Model {
+
+    // No specific attributes needed for calculations
+}
