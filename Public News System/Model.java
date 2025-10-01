8cb4be408a8717bb8c562a9968d587f65c354b68
CodingZap <help@codingzap.com>
2025-10-01 13:21:34 +0530

    Create Model.java

---
+++ Public News System/Model.java
@@ -0,0 +1,52 @@
+package com.example.publicnews.model;
+
+/**
+ * Model class representing a news article.
+ */
+public class Model {
+    private String title;
+    private String content;
+    private String author;
+    private String date;
+
+    // Constructor
+    public Model(String title, String content, String author, String date) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.date = date;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
