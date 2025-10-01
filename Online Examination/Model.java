7b3b0b452294459eab82f1f76cd51136377b71cf
CodingZap <help@codingzap.com>
2025-10-01 13:06:15 +0530

    Create Model.java

---
+++ Online Examination/Model.java
@@ -0,0 +1,42 @@
+package com.example.onlineexam.model;
+
+/**
+ * Model class representing a question in the exam.
+ */
+public class Model {
+    private String question;
+    private String[] options;
+    private int correctAnswerIndex;
+
+    // Constructor
+    public Model(String question, String[] options, int correctAnswerIndex) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    // Getters and Setters
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public void setOptions(String[] options) {
+        this.options = options;
+    }
+
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+
+    public void setCorrectAnswerIndex(int correctAnswerIndex) {
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+}
