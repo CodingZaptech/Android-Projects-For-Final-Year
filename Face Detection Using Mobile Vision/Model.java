c5526d4441a55c46aae2d27f7f2b9e51d060854d
CodingZap <help@codingzap.com>
2025-10-01 12:58:54 +0530

    Create Model.java

---
+++ Face Detection Using Mobile Vision/Model.java
@@ -0,0 +1,62 @@
+package com.example.facedetection.model;
+
+/**
+ * Model class representing detected face information.
+ */
+public class Model {
+    private int id;
+    private float x;
+    private float y;
+    private float width;
+    private float height;
+
+    // Constructor
+    public Model(int id, float x, float y, float width, float height) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
